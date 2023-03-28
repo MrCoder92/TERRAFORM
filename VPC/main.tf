62cd1eaa78fa92f8919ddd6355bdca68d72699cc
George Asiedu <georgeasiedu247@gmail.com>
2023-03-28 20:17:07 +0100

    Init

---
+++ VPC/main.tf
@@ -0,0 +1,99 @@
+terraform {
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>4.2"
+        }
+    }
+}
+
+#configure the AWS
+
+provider "aws"  {
+    region = "eu-west-1"
+}
+
+#Create our first VPC
+resource "aws_vpc" "MyTerraform-VPC"{
+    cidr_block = var.cidr_block[0]
+    tags = {
+        Name = "MyTerraform-VPC"
+    }
+}
+
+#create subnet for VPC
+resource "aws_subnet" "MyLab-Subnet" {
+    vpc_id = aws_vpc.MyTerraform-VPC.id
+    cidr_block = var.cidr_block[1]
+
+    tags = {
+        Name = "MyLab-Subnet"
+    }
+}
+
+#create Internet Gateway
+resource "aws_internet_gateway" "MyIGW" {
+    vpc_id = aws_vpc.MyTerraform-VPC.id
+    tags = {
+        Name = "MyIGW"
+    }
+}
+
+#create Security Group
+resource "aws_security_group" "MyLab_Sec_Grp" {
+    name = "MyLab Security Group"
+    description = "To allow inbound and outbound traffic"
+    vpc_id = aws_vpc.MyTerraform-VPC.id
+    dynamic_ingress{
+        iterator = port
+        for_each = var.ports
+    }
+    content {
+        from_port = port.value
+        to_port = port.value
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress{
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "Allow Traffic"
+    }
+}
+
+# Create Route table and associate it with subnet
+resource "aws_route_table" "MyLab_RouteTable" {
+    vpc_id = aws_vpc.MyLab-VPC.id
+    route{
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.MyIGW.id
+    }
+    tags = {
+        Name = "MyRouteTable"
+}
+}
+
+#create association with subnet
+resource "aws_route_table_association" "MyLab_Association" {
+    subnet_id = aws_subnet.MyLab-Subnet.id
+    route_table_id = aws_route_table.MyLab_RouteTable.id
+}
+
+# Create EC2 Instances
+resource "aws_instance" "MyNewInstance" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = "key-demo"
+    vpc_security_group_ids = [aws_security_group.MyLab_Sec_Grp.id]
+    subnet_id = aws_subnet.MyLab-Subnet.id
+    associate_public_ip_address = true
+
+    tags = {
+        Name = "MyNewInstance"
+    }
+
+}
